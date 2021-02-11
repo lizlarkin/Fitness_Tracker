ac77f514a893e5671340a6cfc6cb6b35bd1a9859
Liz <lizlarkin@highlandtechnology.com>
2021-02-10 21:31:22 -0800

    added total duration using aggregate

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,12 +3,30 @@ const router = require('express').Router();
 // Import Workout Model
 const Workout = require('../models/workout.js');
 
-// Get Workout Range 
+// Get Total Duration of Workouts 
 router.get('/api/workouts', (req, res) => {        
   Workout.aggregate([
     {
       $addFields: {
         totalDuration: {$sum: '$exercises.duration'}
+      }
+    }
+  ])
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+      console.log(dbWorkout)
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+// Get Combined Weight of Workouts 
+router.get('/api/workouts', (req, res) => {        
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalWeight: {$sum: '$exercises.weight'}
       }
     }
   ])
@@ -79,11 +97,3 @@ router.post('/api/workouts/', (req, res) => {
 // });
 
 module.exports = router;
-
-// Add exercises to the most recent workout plan.
-
-// Add new exercises to a new workout plan.
-
-// View the combined weight of multiple exercises from the past seven workouts on the stats page.
-
-// View the total duration of each workout from the past seven workouts on the stats page.

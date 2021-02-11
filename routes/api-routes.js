a39343b348751e7c70ae9f7cc8febd5e90af7fdd
Liz <lizlarkin@highlandtechnology.com>
2021-02-10 16:33:50 -0800

    added api route to get stats for last workout

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,19 +3,33 @@ const router = require('express').Router();
 // Import Workout Model
 const Workout = require('../models/workout.js');
 
-// Get Workout Stats
-router.get('/api/stats', (req, res) => {        
-  Workout.find({}, (err, data) => {
-    if (err) {
-      res.send(err);
-    } else {
-      res.json(data);
-    }
-  });
+// Get Last Workout Stats 
+router.get('/api/workouts', (req, res) => {        
+  Workout.find({})
+    // sort ascending to get most recent workout
+    .sort({ day: 1 })  
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+
+router.get("/api/transaction", (req, res) => {
+  Transaction.find({})
+    .sort({ date: -1 })
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
 });
 
 // Post New Workout
-router.post('/api/exercise', (req, res) => {
+router.post('/api/workouts', (req, res) => {
     Workout.insertOne(req.body)
       .then(dbWorkout => {               
         res.json(dbWorkout);
@@ -45,3 +59,11 @@ router.post('/api/exercise', (req, res) => {
 // });
 
 module.exports = router;
+
+// Add exercises to the most recent workout plan.
+
+// Add new exercises to a new workout plan.
+
+// View the combined weight of multiple exercises from the past seven workouts on the stats page.
+
+// View the total duration of each workout from the past seven workouts on the stats page.
